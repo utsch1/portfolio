b5cb70bcd1afb091b80a2f41f1a112c8a6833dbe
Ute <utegreiner93@gmail.com>
2023-01-06 16:23:53 +0100

    Add tech stack logos and styling

--- src/About.js
+++ src/About.js
@@ -1,13 +1,30 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import afterEffects from './Logos/aftereffects.png';
+import cloudinary from './Logos/cloudinary.png';
 import css3 from './Logos/css3.png';
+import migrations from './Logos/datamigrations.png';
+import drawsql from './Logos/drawsql.png';
+import figma from './Logos/figma.png';
+import flyio from './Logos/flyio.svg';
+import git from './Logos/git.png';
+import github from './Logos/github.png';
 import html5 from './Logos/html5.png';
+import illustrator from './Logos/illustrator.png';
+import indesign from './Logos/indesign.png';
 import javascript from './Logos/javascript.png';
+import jest from './Logos/jest.png';
+import netlify from './Logos/netlify.png';
 import nextjs from './Logos/nextjs.png';
 import nodejs from './Logos/nodejs.png';
+import photoshop from './Logos/photoshop.png';
+import playwright from './Logos/playwright.png';
 import postgresql from './Logos/postgre.png';
 import react from './Logos/react.png';
+import restapi from './Logos/restapi.png';
 import typescript from './Logos/typescript.png';
+import typo3 from './Logos/typo3.png';
+import vscode from './Logos/vscode.png';
 
 const about = css`
   background-color: #ffd447;
@@ -42,21 +59,12 @@ const introduction = css`
   }
 `;
 
-const techStack = css`
-  display: flex;
-`;
-
 const technologies = css`
-  width: 50%;
   display: flex;
   align-items: flex-start;
   justify-content: flex-start;
   flex-wrap: wrap;
-
-  h3 {
-    text-align: center;
-    min-width: 100%;
-  }
+  font-size: 14px;
 
   div {
     color: #fff;
@@ -70,7 +78,7 @@ const technologies = css`
   div > img {
     width: auto;
     height: 20px;
-    margin-right: 10px;
+    margin-right: 7px;
   }
 `;
 
@@ -101,89 +109,208 @@ export function About() {
         </div>
       </div>
 
-      <h2>Tech Skills</h2>
-      <div css={techStack}>
-        <div css={technologies}>
-          <h3>Technologies</h3>
-          <div
-            css={{
-              backgroundColor: '#e34c26',
-            }}
-          >
-            <img src={html5} alt="HTML5 Logo" />
-            HTML5
-          </div>
-          <div
-            css={{
-              backgroundColor: '#264de4',
-            }}
-          >
-            <img src={css3} alt="CSS3 Logo" />
-            CSS3
-          </div>
-          <div
-            css={{
-              backgroundColor: '#323330',
-            }}
-          >
-            <img src={javascript} alt="JavaScript Logo" />
-            JAVASCRIPT
-          </div>
-          <div
-            css={{
-              backgroundColor: '#000',
-            }}
-          >
-            <img src={typescript} alt="TypeScript Logo" />
-            TYPESCRIPT
-          </div>
-          <div
-            css={{
-              backgroundColor: '#336791',
-            }}
-          >
-            <img src={postgresql} alt="postgreSQL Logo" />
-            POSTGRESQL
-          </div>
-          <div
-            css={{
-              backgroundColor: '#3c873a',
-            }}
-          >
-            <img src={nodejs} alt="nodejs Logo" />
-            NODE.JS
-          </div>
-          <div
-            css={{
-              backgroundColor: '#61DBFB',
-            }}
-          >
-            <img src={react} alt="react Logo" />
-            REACT
-          </div>
-          <div
-            css={{
-              backgroundColor: '#000',
-            }}
-          >
-            <img src={nextjs} alt="nextjs Logo" />
-            NEXT.JS
-          </div>
+      <h2>Tech Stack</h2>
+      <div css={technologies}>
+        <div
+          css={{
+            backgroundColor: '#e34c26',
+          }}
+        >
+          <img src={html5} alt="HTML5 Logo" />
+          HTML5
         </div>
-        <h3>Tools</h3>
-        <ul>
-          <li>Git, GitHub</li>
-          <li>Testing (Jest, Playwright)</li>
-          <li>Deployment (fly.io, Netlify)</li>
-          <li>VS Code</li>
-          <li>Typo3</li>
-          <li>DrawSQL</li>
-          <li>Figma</li>
-          <li>Adobe InDesign</li>
-          <li>Adobe PhotoShop</li>
-          <li>Adobe Illustrator</li>
-          <li>Adobe AfterEffects</li>
-        </ul>
+        <div
+          css={{
+            backgroundColor: '#264de4',
+          }}
+        >
+          <img src={css3} alt="CSS3 Logo" />
+          CSS3
+        </div>
+        <div
+          css={{
+            backgroundColor: '#323330',
+          }}
+        >
+          <img src={javascript} alt="JavaScript Logo" />
+          JAVASCRIPT
+        </div>
+        <div
+          css={{
+            backgroundColor: '#00273F',
+          }}
+        >
+          <img src={typescript} alt="TypeScript Logo" />
+          TYPESCRIPT
+        </div>
+        <div
+          css={{
+            backgroundColor: '#336791',
+          }}
+        >
+          <img src={postgresql} alt="postgreSQL Logo" />
+          POSTGRESQL
+        </div>
+        <div
+          css={{
+            backgroundColor: '#292929',
+          }}
+        >
+          <img src={migrations} alt="Database Migration Icon" />
+          DATABASE MIGRATIONS
+        </div>
+        <div
+          css={{
+            backgroundColor: '#000',
+          }}
+        >
+          <img src={restapi} alt="REST API icon" />
+          REST API
+        </div>
+        <div
+          css={{
+            backgroundColor: '#3c873a',
+          }}
+        >
+          <img src={nodejs} alt="nodejs Logo" />
+          NODE.JS
+        </div>
+        <div
+          css={{
+            backgroundColor: '#61DBFB',
+          }}
+        >
+          <img src={react} alt="react Logo" />
+          REACT
+        </div>
+        <div
+          css={{
+            backgroundColor: '#000',
+          }}
+        >
+          <img src={nextjs} alt="nextjs Logo" />
+          NEXT.JS
+        </div>
+        <div
+          css={{
+            backgroundColor: '#3448c5',
+          }}
+        >
+          <img src={cloudinary} alt="cloudinary Logo" />
+          CLOUDINARY
+        </div>
+        <div
+          css={{
+            backgroundColor: '#F1502F',
+          }}
+        >
+          <img src={git} alt="Git Logo" />
+          GIT
+        </div>
+        <div
+          css={{
+            backgroundColor: '#6e5494',
+          }}
+        >
+          <img src={github} alt="Github Logo" />
+          GITHUB
+        </div>
+        <div
+          css={{
+            backgroundColor: '#000',
+          }}
+        >
+          <img src={jest} alt="Jest Logo" />
+          JEST
+        </div>
+        <div
+          css={{
+            backgroundColor: '#292929',
+          }}
+        >
+          <img src={playwright} alt="Playwright Logo" />
+          PLAYWRIGHT
+        </div>
+        <div
+          css={{
+            backgroundColor: '#292929',
+          }}
+        >
+          <img src={flyio} alt="Fly.io Logo" />
+          FLY.IO
+        </div>
+        <div
+          css={{
+            backgroundColor: '#20c6b7',
+          }}
+        >
+          <img src={netlify} alt="Netlify Logo" />
+          NETLIFY
+        </div>
+        <div
+          css={{
+            backgroundColor: '#0078d7',
+          }}
+        >
+          <img src={vscode} alt="VS Code Logo" />
+          VS CODE
+        </div>
+        <div
+          css={{
+            backgroundColor: '#ff8700',
+          }}
+        >
+          <img src={typo3} alt="Typo3 Logo" />
+          TYPO3
+        </div>
+        <div
+          css={{
+            backgroundColor: '#46eacb',
+          }}
+        >
+          <img src={drawsql} alt="DrawSQL Logo" />
+          DRAWSQL
+        </div>
+        <div
+          css={{
+            backgroundColor: '#a259ff',
+          }}
+        >
+          <img src={figma} alt="figma Logo" />
+          FIGMA
+        </div>
+        <div
+          css={{
+            backgroundColor: '#40D0FB',
+          }}
+        >
+          <img src={photoshop} alt="Adobe Photoshop Logo" />
+          ADOBE PHOTOSHOP
+        </div>
+        <div
+          css={{
+            backgroundColor: '#FF3366',
+          }}
+        >
+          <img src={indesign} alt="Adobe Indesign Logo" />
+          ADOBE INDESIGN
+        </div>
+        <div
+          css={{
+            backgroundColor: '#f8a829',
+          }}
+        >
+          <img src={illustrator} alt="Adobe Illustrator Logo" />
+          ADOBE ILLUSTRATOR
+        </div>
+        <div
+          css={{
+            backgroundColor: '#8781bd',
+          }}
+        >
+          <img src={afterEffects} alt="Adobe AfterEffects Logo" />
+          ADOBE AFTER EFFECTS
+        </div>
       </div>
       <h2>My Passions</h2>
       <h3>Sports</h3>
--- src/Icons.js
+++ src/Icons.js
@@ -1,18 +1,124 @@
-
-export function Icons(){
-  return(
-    <p>HTML5</p>
-<a href="https://www.flaticon.com/free-icons/html" title="html icons">Html icons created by Freepik - Flaticon</a>
-<p>CSS3</p>
-<a href="https://www.flaticon.com/free-icons/css" title="css icons">
-    Css icons created by Pixel perfect - Flaticon</a>
-    <a href="https://www.flaticon.com/free-icons/postgre" title="postgre icons">Postgre icons created by Freepik - Flaticon</a>
-    <a href="https://www.flaticon.com/free-icons/react" title="react icons">React icons created by Freepik - Flaticon</a>
-    <a href="https://www.flaticon.com/free-icons/javascript" title="javascript icons">Javascript icons created by Freepik - Flaticon</a>
-    <a href="https://www.flaticon.com/free-icons/typescript" title="typescript icons">Typescript icons created by Freepik - Flaticon</a>
-    <a href="https://www.flaticon.com/free-icons/github" title="github icons">Github icons created by Pixel perfect - Flaticon</a>
-    <a href="https://www.flaticon.com/de/kostenlose-icons/photoshop" title="photoshop Icons">Photoshop Icons erstellt von Freepik - Flaticon</a>
-    <a href="https://www.flaticon.com/de/kostenlose-icons/indesign" title="indesign Icons">Indesign Icons erstellt von Freepik - Flaticon</a>
-    <a href="https://www.flaticon.com/de/kostenlose-icons/adobe" title="adobe Icons">Adobe Icons erstellt von Maniprasanth - Flaticon</a>
+export function Icons() {
+  return (
+    <div>
+      <a
+        target="_blank"
+        href="https://icons8.com/icon/S1YU5GGgIQXW/html-5-is-a-software-solution-stack-that-defines-the-properties-and-behaviors-of-web-page"
+      >
+        HTML 5 is a software solution stack that defines the properties and
+        behaviors of web page
+      </a>{' '}
+      icon by{' '}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+      <a target="_blank" href="https://icons8.com/icon/38272/css3">
+        CSS3
+      </a>{' '}
+      icon by{' '}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+      <a target="_blank" href="https://icons8.com/icon/25010/postgresql">
+        PostgreSQL
+      </a>{' '}
+      icon by{' '}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+      <a href="https://www.flaticon.com/free-icons/react" title="react icons">
+        React icons created by Freepik - Flaticon
+      </a>
+      <a
+        href="https://www.flaticon.com/free-icons/javascript"
+        title="javascript icons"
+      >
+        Javascript icons created by Freepik - Flaticon
+      </a>
+      <a target="_blank" href="https://icons8.com/icon/cHBUT9SmrD2V/typescript">
+        TypeScript
+      </a>{' '}
+      icon by{' '}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+      <a target="_blank" href="https://icons8.com/icon/62856/github">
+        GitHub
+      </a>{' '}
+      icon by{' '}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+      <a
+        href="https://www.flaticon.com/de/kostenlose-icons/photoshop"
+        title="photoshop Icons"
+      >
+        Photoshop Icons erstellt von Freepik - Flaticon
+      </a>
+      <a
+        href="https://www.flaticon.com/de/kostenlose-icons/indesign"
+        title="indesign Icons"
+      >
+        Indesign Icons erstellt von Freepik - Flaticon
+      </a>
+      <a
+        href="https://www.flaticon.com/de/kostenlose-icons/adobe"
+        title="adobe Icons"
+      >
+        Adobe Icons erstellt von Maniprasanth - Flaticon
+      </a>
+      <a target="_blank" href="https://icons8.com/icon/GflC6KLkdd0Y/figma">
+        Figma
+      </a>{' '}
+      icon by{' '}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+      <a
+        target="_blank"
+        href="https://icons8.com/icon/LzPmtqpTuLfm/data-migration"
+      >
+        Data Migration
+      </a>{' '}
+      icon by{' '}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+      <a
+        target="_blank"
+        href="https://icons8.com/icon/Z02xAfOpO5Gg/typo3-a-free-and-open-source-web-content-management-system-written-in-php"
+      >
+        TYPO3 a free and open-source web content management system written in
+        PHP
+      </a>{' '}
+      icon by{' '}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+      <a
+        target="_blank"
+        href="https://icons8.com/icon/WwpSBtoaopNz/jest-can-collect-code-coverage-information-from-entire-projects"
+      >
+        Jest can collect code coverage information from entire projects
+      </a>{' '}
+      icon by{' '}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+      <a target="_blank" href="https://icons8.com/icon/FQlr_bFSqEdG/node-js">
+        Node Js
+      </a>{' '}
+      icon by{' '}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+      <a target="_blank" href="https://icons8.com/icon/35989/react-native">
+        React Native
+      </a>{' '}
+      icon by{' '}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+    </div>
   );
 }
